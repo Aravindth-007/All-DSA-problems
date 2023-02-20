b60437dcd2ea886cf34b8c4dbcf13d7093e61c54
Aravindth-007 <118426062+Aravindth-007@users.noreply.github.com>
2023-02-20 16:52:40 +0530

    Create level_check.js

---
+++ level_check.js
@@ -0,0 +1,50 @@
+// 1. Given the length and breadth , let l = 10; let b = 5; your code should print "Area of the rectangle is 50".
+// let i = 5;
+// let b = 10;
+// let str = "Area of the rectangle is";
+
+// console.log(str  + " " + [i * b]);
+
+
+// 2. Given 2 numbers it should print if they are equal or not equal.
+
+// let a = 10;
+// let b = 20;
+
+// if (a == b) {
+//   console.log("a is equal to b");
+// } else {
+//   console.log("a is not equal to b");
+// }
+
+// 3. For a given positive integer n, it should print every number from 1 to n except x
+
+// let n = 10;
+// let x = 5;
+// let str = "";
+
+// for (let i = 1; i <= n; i++) {
+//   if (i !== x) {
+//     str += i + " ";
+//   }
+// }
+// console.log(str);
+
+// 4. Given an array, print the sum of all even numbers in the array
+// let a = [4, 3, 17, 10, 11];
+
+// let count_value = 0;
+// let array = [4, 3, 17, 10, 11];
+// for(let i = 0;i <array.length;i++){
+//     if(array[i] % 2 == 0){
+//         count_value += array[i];
+//     }
+// }
+// console.log(count_value);
+
+// 5. Given an array of length n and k (k < n) find the maximum sum of k consecutive numbers.
+// for eg:
+// let a = [4,5, 10, 11, 1];
+// let k = 3;
+// The maximum is 26.
+
