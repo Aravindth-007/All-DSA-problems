0c5ec5cdd81af3ffaf41deaced34c5ffba4f42bb
Aravindth-007 <118426062+Aravindth-007@users.noreply.github.com>
2023-02-20 20:00:51 +0530

    Update level_check.js

--- level_check.js
+++ level_check.js
@@ -1,46 +1,45 @@
 // 1. Given the length and breadth , let l = 10; let b = 5; your code should print "Area of the rectangle is 50".
-// let i = 5;
-// let b = 10;
-// let str = "Area of the rectangle is";
+let i = 5;
+let b = 10;
+let str = "Area of the rectangle is";
 
-// console.log(str  + " " + [i * b]);
-
+console.log(str  + " " + [i * b]);
 
 // 2. Given 2 numbers it should print if they are equal or not equal.
 
-// let a = 10;
-// let b = 20;
+let a = 10;
+let m = 20;
 
-// if (a == b) {
-//   console.log("a is equal to b");
-// } else {
-//   console.log("a is not equal to b");
-// }
+if (a == m) {
+  console.log("a is equal to b");
+} else {
+  console.log("a is not equal to b");
+}
 
 // 3. For a given positive integer n, it should print every number from 1 to n except x
 
-// let n = 10;
-// let x = 5;
-// let str = "";
+let n = 10;
+let x = 5;
+let ntr = "";
 
-// for (let i = 1; i <= n; i++) {
-//   if (i !== x) {
-//     str += i + " ";
-//   }
-// }
-// console.log(str);
+for (let i = 1; i <= n; i++) {
+  if (i !== x) {
+    ntr += i + " ";
+  }
+}
+console.log(ntr);
 
 // 4. Given an array, print the sum of all even numbers in the array
 // let a = [4, 3, 17, 10, 11];
 
-// let count_value = 0;
-// let array = [4, 3, 17, 10, 11];
-// for(let i = 0;i <array.length;i++){
-//     if(array[i] % 2 == 0){
-//         count_value += array[i];
-//     }
-// }
-// console.log(count_value);
+let count_value = 0;
+let array = [4, 3, 17, 10, 11];
+for(let i = 0;i <array.length;i++){
+    if(array[i] % 2 == 0){
+        count_value += array[i];
+    }
+}
+console.log(count_value);
 
 // 5. Given an array of length n and k (k < n) find the maximum sum of k consecutive numbers.
 // for eg:
@@ -48,3 +47,16 @@
 // let k = 3;
 // The maximum is 26.
 
+let h = [4, 5, 10, 11, 1];
+let k = 3;
+let maxcount = 0;
+for (let i = 0; i <= h.length - k; i++) {
+  let count = 0;
+  for (let j = i; j < k + i; j++) {
+    count += h[j];
+  }
+  if (count > maxcount) {
+      maxcount = count;
+  }
+}
+console.log(maxcount);
